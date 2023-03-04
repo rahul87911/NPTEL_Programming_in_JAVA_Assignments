c3ead55162f59f2596d29df0810f585223ffdb81
Rahul Mishra <afcatrahul870911@gmail.com>
2023-03-04 06:36:59 -0800

    Week  6 added

---
+++ Week6/Programming_Assignment_02 .java
@@ -0,0 +1,15 @@
+public class Question62 implements Runnable {  
+  
+  @Override  
+  public void run() {  
+      System.out.print(Thread.currentThread().getName()+" has ended.");  
+  }
+// Create main() method and appropriate statements in it
+public static void main(String[] args) {  
+  Question62 ex = new Question62();  
+  Thread t1= new Thread(ex);  
+  t1.setName("Main Thread");
+  t1.start();  
+  System.out.println("Welcome to Java Week 6 New Question.");  
+  t1.setName("Main Thread");
+}
